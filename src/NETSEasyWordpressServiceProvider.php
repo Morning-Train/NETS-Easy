8def2757f6308c1e0e3e15d118449de4677ca5d3
Matb <matb@morningtrain.dk>
2025-05-23 12:54:46 +0200

    🐛 Fix problem, where migrations wasn't running

--- src/NETSEasyWordpressServiceProvider.php
+++ src/NETSEasyWordpressServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace Morningtrain\NETSEasy;
 
+use Illuminate\Support\Str;
 use Morningtrain\NETSEasy\Contracts\PaymentHandleWebhook;
 
 class NETSEasyWordpressServiceProvider extends NETSEasyServiceProvider
@@ -10,6 +11,26 @@ class NETSEasyWordpressServiceProvider extends NETSEasyServiceProvider
     {
         $this->app->singleton(\Morningtrain\NETSEasy\Wordpress\Http\PaymentHandleWebhook::class);
         $this->app->alias(\Morningtrain\NETSEasy\Wordpress\Http\PaymentHandleWebhook::class, PaymentHandleWebhook::class);
+
+        if ($this->app->runningInConsole()) {
+            $argv = $_SERVER['argv'] ?? [];
+
+            /**
+             * Check if we are running a migration command
+             * Example: [
+             *  "/usr/local/bin/wp"
+             *  "--path=/app/wordpress"
+             *  "artisan"
+             *  "migrate"
+             * ]
+             */
+            if (
+                ! empty($argv[2]) && $argv[2] == 'artisan' &&
+                ! empty($argv[3]) && Str::contains($argv[3],'migrate')
+            ) {
+                $this->package->runsMigrations();
+            }
+        }
 
         parent::bootingPackage();
     }
